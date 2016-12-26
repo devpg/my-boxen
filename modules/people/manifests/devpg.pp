59cf0b1480179e3359a741fbd27d3e668fc38b01
André Neubauer <andre.neubauer@IT-NB2248.fritz.box>
2016-12-26 13:57:27 +0100

    Try out cask integration by installing slack

--- modules/people/manifests/devpg.pp
+++ modules/people/manifests/devpg.pp
@@ -1,7 +1,10 @@
 class people::devpg {
-  
+
   # atom includes some additional packages
   include atom
   atom::package { 'linter': }
   atom::theme { 'monokai': }
+
+  # apps via cask
+  package { 'slack': provider => 'brewcask' }
 }
